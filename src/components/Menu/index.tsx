e2057ae471189f0dab39375031e083a4c7f7f375
Ravikash Gupta <98596722+ravikaashg@users.noreply.github.com>
2022-07-03 14:12:22 -0600

    Update index.tsx

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -258,7 +258,7 @@ export default function Menu() {
           <Trans>Referral</Trans>
         </NavMenuItem>
         {!above1100 && (
-          <MenuItem id="link" href={DMM_ANALYTICS_URL[chainId as ChainId]}>
+          <MenuItem id="link" href={]}>
             <PieChart size={14} />
             <Trans>Analytics</Trans>
           </MenuItem>
